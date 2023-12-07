5692e1db6099237a83e91dcebe386dd7e6a065d6
elem3d <elem3d@users.noreply.github.com>
2023-12-07 20:03:13 -0300

    -v beta

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,6 +4,7 @@ const state = {
         timeLeft: document.querySelector("#time"),
         score: document.querySelector("#score"),
         squares: document.querySelectorAll(".square"),
+        level: document.querySelector('#level')
     },
     value: {
         gameVelocity: 1000,
@@ -11,6 +12,7 @@ const state = {
         result: 0,
         currentTime: 60,
         level: 0,
+        error: 0,
     },
 
     actions: {
@@ -31,51 +33,86 @@ function randomSquare() {
     state.value.hitPosition = randomSquare.id
 }
 
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.mp3`)
+    audio.play();
+    audio.playbackRate = 3;
+   }
+   function playLost(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.mp3`)
+    audio.play();
+    audio.playbackRate = 1;
+   }
+
 function countDown(){
     state.value.currentTime--;
     state.view.timeLeft.textContent = state.value.currentTime;
 
     if(state.value.currentTime <= 0) {
-        alert("TIME OUT - GAME OVER! Your Score: " + state.value.result);
+        if (state.value.result < 25){
+            alert('TIME OUT! Your Score: ' + state.value.result);
+        } else {
+            alert('TIME OUT! Your Score:' + state.value.result + 'CONGRATULATIONS!');
+        }
         clearInterval(state.actions.countDownTimerId);
         clearInterval(state.actions.timerId);
+        playLost('gameOver');
     }
 }
 
 function moveEnemy() {
-    state.value.timerId = setInterval(randomSquare, state.value.gameVelocity)
+    state.value.timerId = setInterval(randomSquare, state.value.gameVelocity);
 }
 
 function addListnerHitBox() {
     state.view.squares.forEach((square) => {
-        square.addEventListener("mousedown", () => { 
-            if(square.id === state.value.hitPosition){
+        square.addEventListener('mousedown', () => { 
+            if(square.id === state.value.hitPosition && state.value.result <= 30){
                 state.value.result++;
                 state.view.score.textContent = state.value.result;
                 state.value.hitPosition = null;
-
+                playSound('hit');
+            
+            } else if(square.id === state.value.hitPosition && state.value.result > 30){
+                state.value.result = state.value.result + 5;
+                state.view.score.textContent = state.value.result;
+                state.value.hitPosition = null;
+                playSound('hit');
+            
+            } else {
+                state.value.error++
+                playLost('chipmunk');
             }
         })
     });
 }
 
-function levelUp(){
-    state.value.gameVelocity = state.value.gameVelocity / 2;
-    state.value.level = state.value.level + 5;
-    alert("LEVEL UP!")
+function levelUp(score){
+    if(score == 10) {
+        alert('LEVEL UP!');
+        state.value.gameVelocity = 800;
+        state.value.level++;
+    }else if (score == 20) {
+        alert('LEVEL UP!')
+        state.value.gameVelocity = 700;
+        state.value.level++;
+    } else if (score == 30) {
+        alert('LEVEL UP MAX!')
+        state.value.gameVelocity = 500;
+        state.value.level++;
+    }
+
+    state.view.level.textContent = state.value.level;
 }
 
-function playSound(audioName) {
- let audio = new Audio(`../audios/${audioName}.mp3`);
- audio.play();
+function lifeCount(error) {
+
 }
 
 function init() {
     addListnerHitBox();
     moveEnemy();
-    if (state.value.result % 3 === 0) {
-        levelUp();
-    }
+    levelUp(state.value.result);
 }
 
 init();
