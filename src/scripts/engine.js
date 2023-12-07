56d94ace5e272e4653bdcd8d5f9a6d2e79db2f4e
elem3d <elem3d@users.noreply.github.com>
2023-12-07 17:44:09 -0300

    audio adjust

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -2,12 +2,20 @@ const state = {
     view: {
         enemy: document.querySelector(".enemy"),
         timeLeft: document.querySelector("#time"),
-        sore: document.querySelector("#score"),
+        score: document.querySelector("#score"),
         squares: document.querySelectorAll(".square"),
     },
     value: {
-        timerId: null,
         gameVelocity: 1000,
+        hitPosition: 0, 
+        result: 0,
+        currentTime: 60,
+        level: 0,
+    },
+
+    actions: {
+        timerId: null,
+        countDownTimerId: setInterval(countDown, 1000),
     }
 }
 
@@ -20,6 +28,18 @@ function randomSquare() {
     let randomSquare = state.view.squares[randomNumber];
 
     randomSquare.classList.add("enemy");
+    state.value.hitPosition = randomSquare.id
+}
+
+function countDown(){
+    state.value.currentTime--;
+    state.view.timeLeft.textContent = state.value.currentTime;
+
+    if(state.value.currentTime <= 0) {
+        alert("TIME OUT - GAME OVER! Your Score: " + state.value.result);
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+    }
 }
 
 function moveEnemy() {
@@ -28,11 +48,34 @@ function moveEnemy() {
 
 function addListnerHitBox() {
     state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => { 
+            if(square.id === state.value.hitPosition){
+                state.value.result++;
+                state.view.score.textContent = state.value.result;
+                state.value.hitPosition = null;
+
+            }
+        })
     });
 }
 
+function levelUp(){
+    state.value.gameVelocity = state.value.gameVelocity / 2;
+    state.value.level = state.value.level + 5;
+    alert("LEVEL UP!")
+}
+
+function playSound(audioName) {
+ let audio = new Audio(`../audios/${audioName}.mp3`);
+ audio.play();
+}
+
 function init() {
-    moveEnemy()
+    addListnerHitBox();
+    moveEnemy();
+    if (state.value.result % 3 === 0) {
+        levelUp();
+    }
 }
 
 init();
