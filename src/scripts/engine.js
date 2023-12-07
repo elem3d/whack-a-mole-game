8aa364c9ac189bcba62eae1a6d772fcc1be806a0
elem3d <elem3d@users.noreply.github.com>
2023-12-07 16:44:02 -0300

    add move enemy function & change enemy border

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,35 +1,38 @@
-const state {
+const state = {
     view: {
-
-        null: document.querySelectorAll(".square"),
         enemy: document.querySelector(".enemy"),
         timeLeft: document.querySelector("#time"),
         sore: document.querySelector("#score"),
-        squares: document.querySelectorAll(".square")
+        squares: document.querySelectorAll(".square"),
+    },
+    value: {
+        timerId: null,
+        gameVelocity: 1000,
     }
-
-    value: {}
 }
 
 function randomSquare() {
     state.view.squares.forEach((square) => {
         square.classList.remove("enemy");
     });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+
+    randomSquare.classList.add("enemy");
+}
+
+function moveEnemy() {
+    state.value.timerId = setInterval(randomSquare, state.value.gameVelocity)
 }
 
 function addListnerHitBox() {
     state.view.squares.forEach((square) => {
     });
-
-    let randomNumber = Math.floor(Math.random() * 9);
-    let randomSquare = state.view.squares[randomNumber];
-
-    randomSquare.classList("enemy");
 }
 
 function init() {
-    alert("Olá!")
-    randomSquare()
+    moveEnemy()
 }
 
 init();
