9f9425e335fd06e973fa7f80581c707be50d09dd
elem3d <elem3d@users.noreply.github.com>
2023-12-10 20:51:53 -0300

    menu html e css

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,3 +1,15 @@
+const cursor1 = document.querySelector('#custom-cursor');
+
+const moveCursor = (e)=> {
+    const mouseY = e.clientY;
+    const mouseX = e.clientX;
+     
+    cursor1.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  }
+  
+  window.addEventListener('mousemove', moveCursor)
+
+
 const state = {
     view: {
         enemy: document.querySelector(".enemy"),
